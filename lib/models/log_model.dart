5d16c4a93421665c9b6a0035baef9f8db9b81d35
biryang <56025411+biryang@users.noreply.github.com>
2021-07-07 04:16:57 +0900

    완료 루틴 기록 모델 생성

---
+++ lib/models/log_model.dart
@@ -0,0 +1,20 @@
+import 'package:hive/hive.dart';
+import 'package:hr_app/models/routine_model.dart';
+
+part 'log_model.g.dart';
+
+@HiveType(typeId: 5)
+class LogModel {
+  @HiveField(0)
+  DateTime dateTime;
+  @HiveField(1)
+  int totalTime;
+  @HiveField(2)
+  RoutineModel routineModel;
+
+  LogModel({
+    this.dateTime,
+    this.totalTime,
+    this.routineModel,
+  });
+}
